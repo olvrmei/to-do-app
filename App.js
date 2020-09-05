fe3f610842d88653ca142c1eb67794c61e08a5d4
Meika Oliveira <meikafoliveira@gmail.com>
2020-09-05 12:44:10 -0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,197 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+//import { StatusBar } from 'expo-status-bar';
+import React, { useState, useCallback, useEffect } from 'react';
+import { View, Text, StyleSheet, SafeAreaView, StatusBar, TouchableOpacity, FlatList, Modal, TextInput, AsyncStorage } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import TaskList from './src/components/TaskList'
+import * as Animatable from 'react-native-animatable';
+
+const AnimatedBtn = Animatable.createAnimatableComponent(TouchableOpacity);
 
 export default function App() {
+  const [task, setTask] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [input, setInput] = useState('');
+
+  // Buscando todas tarefas ao iniciar o app
+  useEffect(() => {
+
+    async function loadTasks(){
+      const taskStorage = await AsyncStorage.getItem('@task')
+      
+      if(taskStorage){
+        setTask(JSON.parse(taskStorage));
+      }
+    }
+
+    loadTasks();
+
+  }, []);
+
+  // Salvando cado tenha alguma tarefa alterada
+  useEffect(() => {
+    async function saveTasks(){
+      await AsyncStorage.setItem('@task', JSON.stringify(task));
+    }
+
+    saveTasks();
+
+  }, [task]);
+
+  function handleAdd(){
+    if(input === '') return;
+
+    const data = {
+      key: input,
+      task: input
+    };
+
+    setTask([...task, data]);
+    setOpen(false); // fecha aba de nova tarefa
+    setInput(''); // limpa input
+  }
+
+  const handleDelete = useCallback((data) => {
+    const find = task.filter(r => r.key !== data.key);
+    setTask(find);
+  })
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor = "#171d31" barStyle="light-content"/>
+      <View style={styles.content}>
+      <Text style={styles.title}>My To Do List</Text>
+      </View>
+
+      <FlatList
+        marginHorizontal={10}
+        showsHorizontalScrollIndicator={false}
+        data={task}
+        keyExtractor={ (item) => String(item.key) }
+        renderItem={ ({ item }) => <TaskList data={item} handleDelete={handleDelete} />}
+      />
+
+      <Modal 
+      animationType="slide" 
+      transparent={false} 
+      visible={open}
+      >
+        <SafeAreaView style={styles.modal}>
+
+          <View style={styles.modalHeader}>
+            <TouchableOpacity onPress={ () => setOpen(false)}>
+              <Ionicons style={{marginLeft:5, marginRight: 5}} name="md-arrow-back" size={40} color="#000" />
+            </TouchableOpacity>
+            <Text style={styles.modalTitle}>New task</Text>
+          </View>
+
+          <Animatable.View 
+          style={styles.modalBody} 
+          animation="fadeInUp" 
+          useNativeDriver
+          >
+            <TextInput
+            multiline={true}
+            placeholderTextColor="#747474"
+            autoCorrect={false}
+            placeholder="What do you need to do today?"
+            style={styles.input}
+            value={input}
+            onChangeText={ (texto) => setInput(texto) }
+            />
+            <TouchableOpacity style={styles.handleAdd} onPress={handleAdd}>
+              <Text style={styles.handleAddText}>Add task</Text>
+            </TouchableOpacity>
+          </Animatable.View>
+        </SafeAreaView>
+      </Modal>
+
+      <AnimatedBtn 
+      style={styles.fab}
+      useNativeDriver
+      animation="bounceInUp"
+      duration={1500}
+      onPress={ () => setOpen(true)}
+      >
+        <Ionicons name="ios-add" size={35} color="#FFF"/>
+      </AnimatedBtn>
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#171d31',
+    
+  },
+  title: {
+    marginTop: 10,
+    paddingBottom: 10,
+    fontSize: 25,
+    textAlign: 'center',
+    color:'#FFF' 
+  },
+  fab: {
+    position: 'absolute',
+    width: 60,
+    height: 60,
+    backgroundColor: '#b0d0d3',
     alignItems: 'center',
     justifyContent: 'center',
+    borderRadius: 30,
+    right: 25,
+    bottom: 25,
+    elevation: 2,
+    zIndex: 9,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowOffset:{
+      width: 1,
+      height: 3,
+    }
   },
+  modal: {
+    flex:1,
+    backgroundColor: '#171d31',
+  },
+  modalHeader: {
+    marginLeft: 10,
+    marginTop: 20,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  modalTitle: {
+    marginLeft: 15,
+    fontSize: 23,
+    color: '#FFF'
+  },
+  modalBody: {
+    marginTop: 15,
+  },
+  input: {
+    fontSize: 15,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 30,
+    backgroundColor: '#FFF',
+    padding: 9,
+    height: 85,
+    textAlignVertical: 'top',
+    color: '#000',
+    borderRadius: 5,
+  },
+  handleAdd: {
+    backgroundColor: '#FFF',
+    marginTop: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+    height: 40,
+    borderRadius: 5
+  },
+  handleAddText: {
+    fontSize: 20,
+  }
 });
